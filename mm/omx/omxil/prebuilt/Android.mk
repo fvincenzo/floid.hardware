bf666897b8aa3aec452103244957e4f063100181
Ciprian Talaba <ctalaba@windriver.com>
2012-03-23 17:33:55 +0200

    Removing old prebuilt file from makefile
    Signed-off-by: Ciprian Talaba <ctalaba@windriver.com>

--- mm/omx/omxil/prebuilt/Android.mk
+++ mm/omx/omxil/prebuilt/Android.mk
@@ -19,13 +19,5 @@ LOCAL_MODULE_SUFFIX := .so
 LOCAL_SRC_FILES := libhantroimagedec.so
 include $(BUILD_PREBUILT)
 
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libhantro_omx_core
-LOCAL_MODULE_TAGS := debug eng optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_SRC_FILES := libhantro_omx_core.so
-include $(BUILD_PREBUILT)
 endif
 endif
